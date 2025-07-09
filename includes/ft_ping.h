c7b837cc813006d9a3dbc53034febb9365f8a52f
Nouakchi <othman.nouakchi007@gmail.com>
2025-07-09 14:58:02 +0100

    implementing the DNS look-up

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -1,6 +1,15 @@
 #ifndef FT_PING_H
 #define FT_PING_H
 
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
 
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,12 +1,48 @@
 #include "../includes/ft_ping.h"
-#include <stdio.h>
+
 
 int main(int ac, char *av[])
 {
     if (ac < 2)
-        return (printf("ft_ping: usage error: Destination address required\n"), 0);
+        return (printf("ft_ping: usage error: Destination address required\n"), EXIT_FAILURE);
 
-    printf("%s", av[0]);
+    const char *hostname = av[1];
+
+    struct addrinfo hint;
+    struct addrinfo *result;
+
+    memset(&hint, 0, sizeof(hint));
+    hint.ai_family = AF_INET;
+    hint.ai_socktype = SOCK_STREAM;
+    result = NULL;
+
+    int status = getaddrinfo(hostname, NULL, &hint, &result);
+
+    if (status)
+        return (printf("ft_ping: internal error: getaddrinfo failed!\n"), EXIT_FAILURE);
+
+    struct addrinfo *tmp = result;
+    while (tmp != NULL)
+    {
+        printf("Entry:\n");
+        printf("\tType: %i\n", tmp->ai_socktype);
+        printf("\tFamily: %i", tmp->ai_family);
+
+        char adress_string[INET_ADDRSTRLEN];
+        void *addr;
+        if (tmp->ai_family == AF_INET)
+        {
+            addr = &((struct sockaddr_in *)tmp->ai_addr)->sin_addr;
+            inet_ntop(tmp->ai_family, addr, adress_string, INET_ADDRSTRLEN);
+        }
+
+        printf("\tAdress: %s", adress_string);
+        tmp = tmp->ai_next;
+    }
+
+    printf("%d\n", status);
+
+    freeaddrinfo(result);
     
-    return (0);
+    return (EXIT_SUCCESS);
 }
